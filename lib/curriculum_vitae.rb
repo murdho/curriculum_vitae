f0d1db49e03ebad936930819ff81d0e40324942f
Murdho <murdho@murdho.com>
2015-11-25 16:46:34 +0100

    Implement method for simplified usage

--- lib/curriculum_vitae.rb
+++ lib/curriculum_vitae.rb
@@ -2,4 +2,7 @@ require_relative 'curriculum_vitae/version'
 require_relative 'curriculum_vitae/builder'
 
 module CurriculumVitae
+  def self.build(&block)
+    Builder.build(&block)
+  end
 end
--- spec/curriculum_vitae_spec.rb
+++ spec/curriculum_vitae_spec.rb
@@ -4,4 +4,16 @@ describe CurriculumVitae do
   it 'has a version number' do
     expect(CurriculumVitae::VERSION).not_to be nil
   end
+
+  context '.build' do
+    it 'passes block to `Builder.build`' do
+      expect(CurriculumVitae::Builder).to receive(:build).and_call_original
+
+      result = subject.build do
+        code 'Red'
+      end
+
+      expect(result).to eq(code: 'Red')
+    end
+  end
 end
